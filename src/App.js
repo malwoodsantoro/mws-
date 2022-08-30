03286cf361f29a17e13570ca49a02675b08b4d06
mal-wood <mallorywood413@gmail.com>
2022-08-30 16:56:08 -0400

    mobile clean up

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Hero />
       <a href="/#skills" name="skills"><Skills /></a>
       <a href="/#projects" name="projects"><Projects /></a>
-      <div className="bg-[#fff] text-[#fff]"><a href="/#contact" name="contact">Contact</a></div>
+      <div className="bg-[#fff] text-[#fff] outline-none focus:outline-0"><a href="/#contact" name="contact">Contact</a></div>
       <ContactForm/>
       <footer className="text-center lg:text-left">
         <div className="text-gray-700 text-center p-8 bg-[#E5E1E0]">
